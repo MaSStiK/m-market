92d36453cc83f2b8f50a335b9d356570e2ca9bc6
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2024-10-26 00:31:57 +0200

    fix

---
+++ src/components/Localization/RU.js
@@ -0,0 +1,54 @@
+const RU = {
+    Header: {
+        welcome: "Добро пожаловать в интернет-магазин M-Market.",
+        socialMedia: "Наши соц-сети:",
+        searchPlaceholder: "Ищите что-нибудь...",
+        funcCart: "Корзина",
+        funcWishlist: "Избранное",
+        funcDashboard: "Личный кабинет",
+    },
+    Product: {
+        buttonAddToFavorites: "Добавить в избранное",
+        buttonAddToCardText: "В корзину",
+        buttonAddToCard: "Добавить в корзину",
+        buttonCompare: "Сравнить",
+        badge: {
+            hot: "Хит",
+            out: "Нет в наличии",
+            sale: "Распродажа",
+            best: "Лучшие предложение",
+            discount: "Скидка",
+        }
+    },
+    CatalogBestDeals: {
+        title: "Суперцены",
+        bestDealsEnds: "До конца скидок",
+        bestDealsEndsTimer: "16д : 21ч : 57м : 23с",
+    },
+    CatalogTop: {
+        theBestPlaceToPlay: "Лучшее место для игр",
+        xboxConsoles: "Консоли Xbox",
+        saveUp: "Сэкономьте до 50% на выбранных играх для Xbox. Получите 3 месяца подписки на PC Game Pass за 2 доллара США.",
+        summerSales: "Летняя распродажа",
+        newGooglePixel: "Новый Google Pixel 6 Pro",
+        xiaomiFlipBudsPro: "Xiaomi FlipBuds Pro",
+        badge: "29% Скидка",
+        buttonBuy: "Купить",
+        features: {
+            title1: "Быстрая доставка",
+            subTitle1: "Доставка за 24 ч",
+            title2: "Возврат за 24 часа",
+            subTitle2: "100% возврат средств",
+            title3: "Безопасная оплата",
+            subTitle3: "Ваши деньги под защитой",
+            title4: "Поддержка 24/7",
+            subTitle4: "Живой чат"
+        }
+    },
+    Dashboard: {
+        helloUsername: "Здравствуйте, Username",
+        button: "Кнопка"
+    }
+}
+
+export default RU
